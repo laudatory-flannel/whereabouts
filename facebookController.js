5d6770c43ec5a0bc80a727830321c7a66a6449da
Hack Reactor Students <accounts@hackreactor.com>
2016-01-06 19:21:09 -0800

    Implements working Google map (for a random coordinate); has unfinished attempt for getting Facebook friend list

--- facebookController.js
+++ facebookController.js
@@ -1,14 +1,36 @@
 angular.module('facebook', [])
 .controller('facebookController', function($scope) {
+  $scope.getUserData = function() {
+    FB.api('/me', function(response) {
+      console.log("user info:", response);
+      $scope.getFriendLists();
+    })
+  };
+  $scope.getFriendLists = function(authResponse) {
+    FB.api("/me/friends?access_token=" + authResponse, function(response) {
+      console.log("friend list:", response);
+    });
+  };
+  $scope.getAuthResponse = function() {
+    console.log('running this');
+    FB.getAuthResponse(function(response) {
+      console.log("auth response:", response);
+    });
+  };
   $scope.login = function() {
     FB.getLoginStatus(function(response) {
       console.log(response)
       if (response.status === 'connected') {
         console.log('Logged in.');
+        $scope.getUserData(response.authResponse);
+        //$scope.getAuthResponse();
       }
       else {
         FB.login();
       }
     });
   };
+  $scope.logout = function() {
+    FB.logout();
+  }
 });
