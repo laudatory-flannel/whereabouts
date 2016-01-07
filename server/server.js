b7d55593c6882bd20f05ebd1357478e9efc7c9a6
Rachel RoseFigura <rachel.rosefigura@gmail.com>
2016-01-06 17:26:00 -0800

    server and database skeleton

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+// require stuff
+// require('')(app, express);
+
+// start listening to requests on local port or port 8000
+app.listen(process.env.port || process.env.PORT || 8000);
+
+module.exports = app;
