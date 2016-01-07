681ac8fd5728e5528da9b4fd56bd24350a512097
Max O'Connell <moconnell7171@gmail.com>
2016-01-07 13:50:59 -0800

    Routes respond to get/events, post/auth, post/events

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,37 @@
-//var bodyParser = require('body-parser');
+var bodyparser = require('body-parser');
+var database = require('./config/database.js');
 
-module.exports = function(app, express){
+
+module.exports = function(app, express) {
 	app.use('/', express.static('../Client'));
+// Get requests
+	app.get('/events', function(req, res){
+		database.getActiveEvents().then(function(data){
+			res.json(data);
+		});
+	});
+
+
+// Post requests
+	//Posting new event
+	app.post('/auth', function(req, res){
+		database.addUserToDb(req.body, function(err, result){
+			if(err){
+				console.log(err);
+				res.status(500);
+				res.redirect('/home');
+			} else {
+				res.redirect('/home');
+			}
+		});
+		res.redirect('/auth');
+	});
+
+	//Logging in/authentication
+	app.post('/event', function(req, res){
+		//AUTHENTICATION HERE
+		database.addEventToDb(req.body);
+		res.redirect('/home');
+	});
+
 };
