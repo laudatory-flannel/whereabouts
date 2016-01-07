68b2e61833b2e0b4005113bbc988ef335a60bcc6
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-07 11:51:57 -0800

    Moves API-related files into client file structure

--- client/app/home.js
+++ client/app/home.js
@@ -1,3 +1,5 @@
-angular.module('greenfield.home', []).controller('HomeController', function ($scope, $window, $location, Services) {
+var homeModule = angular.module('greenfield.home', ['greenfield.services']);
+// .controller('HomeController', function ($scope, $window, $location, Services) {
    
-});
+// });
+//var homeModule = angular.module('homeModule', ['services']);
