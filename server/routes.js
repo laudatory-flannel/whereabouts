901080ded14ea572b86d478fb3de6c30307ada68
Max O'Connell <moconnell7171@gmail.com>
2016-01-07 16:29:18 -0800

    Conflict removed

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,6 @@
 var bodyparser = require('body-parser');
 var database = require('./config/database.js');
-<<<<<<< HEAD
 var jwt = require('jwt-simple');
-=======
->>>>>>> 681ac8fd5728e5528da9b4fd56bd24350a512097
 
 
 module.exports = function(app, express) {
@@ -19,7 +16,6 @@ module.exports = function(app, express) {
 // Post requests
 	//Posting new event
 	app.post('/auth', function(req, res){
-<<<<<<< HEAD
 		// if logged in with facebook
 		//create token
 		var token = jwt.encode(user, 'candyvan');
@@ -39,29 +35,13 @@ module.exports = function(app, express) {
 			}
 		});
 		// else redirect
-=======
-		database.addUserToDb(req.body, function(err, result){
-			if(err){
-				console.log(err);
-				res.status(500);
-				res.redirect('/home');
-			} else {
-				res.redirect('/home');
-			}
-		});
->>>>>>> 681ac8fd5728e5528da9b4fd56bd24350a512097
 		res.redirect('/auth');
 	});
 
 	//Logging in/authentication
-<<<<<<< HEAD
 	app.post('/events', function(req, res){
 		//AUTHENTICATION HERE
 		//if auth
-=======
-	app.post('/event', function(req, res){
-		//AUTHENTICATION HERE
->>>>>>> 681ac8fd5728e5528da9b4fd56bd24350a512097
 		database.addEventToDb(req.body);
 		res.redirect('/home');
 	});
