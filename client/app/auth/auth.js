2aa153dde33f1052f850675a1e7d3f6d86e9237b
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-10 10:49:30 -0800

    Adds Auth factory in services for modularity and token storage

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,10 +1,14 @@
 angular.module('greenfield.auth', ['greenfield.services'])
-.controller('AuthController', function($scope, HTTP, Facebook) {
+.controller('AuthController', function($scope, $location, Facebook, Auth) {
   $scope.login = function() {
     var loginToApp = function(accessToken, userName) {
-      HTTP.sendRequest('POST', '/auth', {
+      Auth.login({
         accessToken: accessToken,
         userName: userName
+      })
+      .then(function(response) {
+        //should do some check of response statusCode etc. to confirm successful login
+        $location.path('/home');
       });
     };
 
@@ -17,5 +21,10 @@ angular.module('greenfield.auth', ['greenfield.services'])
     });
   };
 
-  $scope.logout = Facebook.logout;
+  $scope.logout = function() {
+    Facebook.logout(function() {
+      Auth.logout();
+      $location.path('/auth');
+    });
+  }
 });
