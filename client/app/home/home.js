505989156a2bf79989e9013a5c904e42a022c436
gdomorski <gregdomorski@gmail.com>
2016-01-10 12:52:24 -0500

    Fix Directions, change drop down for create an event

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -66,6 +66,9 @@ app.controller('HomeController', function($scope, localStorage, $http) {
       // $apply notifies angular to watch changes and re-evaluate ng-if/show expressions
       $scope.loading = false;
     });
+
+    var directionsDisplay = new google.maps.DirectionsRenderer({ draggable: true });
+    var directionsService = new google.maps.DirectionsService();
     $scope.map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: $scope.position[0], lng: $scope.position[1]},
       zoom: 14,
@@ -77,7 +80,17 @@ app.controller('HomeController', function($scope, localStorage, $http) {
       styles: [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
     });
 
-  $scope.findEvents();
+    directionsDisplay.setMap($scope.map);
+
+    directionsDisplay.setPanel(document.getElementById("directions"));
+
+    var onChangeHandler = function() {
+      $scope.calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    document.getElementById('end').addEventListener('change', onChangeHandler);
+
+      $scope.findEvents();
 
     var allLocations = [
     {
@@ -95,7 +108,23 @@ app.controller('HomeController', function($scope, localStorage, $http) {
       timeuntil: "10 p.m.",
       latitude: 37.786710,
       longitude: -122.400831
-    }  
+    },
+    {
+      id: 3,
+      personname: "Gloria Ma",
+      description: "I'm  hanging out at the Hyatt!! Come join me",
+      timeuntil: "8 p.m.",
+      latitude: 37.794301,
+      longitude: -122.39573
+    },
+    {
+      id: 4,
+      personname: "Rachel RoseFigura",
+      description: "I'm at Starbucks Bros!",
+      timeuntil: "8 p.m.",
+      latitude: 37.784118,
+      longitude: -122.406435
+    },    
     ];
 
     var myLocations = [
@@ -113,17 +142,26 @@ app.controller('HomeController', function($scope, localStorage, $http) {
       $scope.postMarker.push(new google.maps.Marker({
         position: new google.maps.LatLng(allLocations[i].latitude, allLocations[i].longitude), 
         animation: google.maps.Animation.DROP,
-        map: $scope.map
+        map: $scope.map,
+        icon: 'app/home/peace.png'
       }));
       
 
       google.maps.event.addListener($scope.postMarker[i], 'click', (function(postMarker, i) {
         return function() {
-          infowindow.setContent(allLocations[i].personname);
+          infowindow.setContent("<h4>" + allLocations[i].personname + "</h4>" + allLocations[i].description + "<p>Will be there until " + allLocations[i].timeuntil + "</p>");
           infowindow.open($scope.map, postMarker);
         };
       })($scope.postMarker[i], i));
     }
+
+    new google.maps.Marker({
+        position: new google.maps.LatLng($scope.position[0], $scope.position[1]), 
+        animation: google.maps.Animation.DROP,
+        map: $scope.map,
+        icon: 'app/home/currentlocation.png'
+      })
+
     
 
   };
@@ -143,100 +181,22 @@ app.controller('HomeController', function($scope, localStorage, $http) {
     });
   };
 
+  $scope.calculateAndDisplayRoute = function (directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: document.getElementById('start').value,
+      destination: document.getElementById('end').value,
+      travelMode: google.maps.TravelMode.WALKING
+    }, function(response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
   $scope.initMap();
 
 
-
-//   $scope.compile = function(element) {
-//     var el = angular.element(element);    
-//     $scope = el.scope();
-//     $injector = el.injector();
-//     $injector.invoke(function($compile){
-//        $compile(el)($scope);
-//     });  
-//   };
-
-//   $scope.open_panel = function(){
-//     $scope.slideIt();
-//     var el = document.getElementById("sidebar");
-//     el.setAttribute("id", "sidebar1");
-//     el.setAttribute("ng-click", "close_panel()");
-//     $scope.compile(el);
-//   };
-
-//   $scope.slideIt = function(){
-//     var slidingDiv = document.getElementById("slider");
-//     var stopPosition = 0;
-//     if (parseInt(slidingDiv.style.right) < stopPosition) {
-//     slidingDiv.style.right = parseInt(slidingDiv.style.right) + 2 + "px";
-//     setTimeout($scope.slideIt, 1);
-//     }
-//   };
-
-//   $scope.slideIn = function(){
-//     var slidingDiv = document.getElementById("slider");
-//     var stopPosition = -342;
-//     if (parseInt(slidingDiv.style.right) > stopPosition) {
-//       slidingDiv.style.right = parseInt(slidingDiv.style.right) - 2 + "px";
-//       setTimeout($scope.slideIn, 1);
-//     }
-//   };
-
-//   $scope.close_panel = function(){
-//     $scope.slideIn();
-//     el = document.getElementById("sidebar1");
-//     el.setAttribute("id", "sidebar");
-//     el.setAttribute("ng-click", "open_panel()");
-//     $scope.compile(el);
-//   };
-
 });
 
-app.controller('MainCtrl', function ($scope) {
-    $scope.showModal = false;
-    $scope.toggleModal = function(){
-        $scope.showModal = !$scope.showModal;
-    };
-  });
-
-app.directive('modal', function () {
-    return {
-      template: '<div class="modal fade">' + 
-          '<div class="modal-dialog">' + 
-            '<div class="modal-content">' + 
-              '<div class="modal-header">' + 
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
-                '<h4 class="modal-title">{{ title }}</h4>' + 
-              '</div>' + 
-              '<div class="modal-body" ng-transclude></div>' + 
-            '</div>' + 
-          '</div>' + 
-        '</div>',
-      restrict: 'E',
-      transclude: true,
-      replace:true,
-      scope:true,
-      link: function postLink(scope, element, attrs) {
-        scope.title = attrs.title;
-
-        scope.$watch(attrs.visible, function(value){
-          if(value == true)
-            $(element).modal('show');
-          else
-            $(element).modal('hide');
-        });
-
-        $(element).on('shown.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = true;
-          });
-        });
-
-        $(element).on('hidden.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = false;
-          });
-        });
-      }
-    };
-  });
