a9b1976bedf0b5f357c26437f71b62557f674ce6
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-09 16:48:18 -0800

    Integrates location autocomplete; cleans code in event view, event controller, and index.html for clarity

--- client/app/event/event.js
+++ client/app/event/event.js
@@ -1,42 +1,27 @@
 angular.module('greenfield.event', [])
-.controller('EventController', function($scope, $http) {
-  $scope.result = "";
-
-  $scope.searchFunc = function (query) {
-    var latitude = "37.783697";
-    var longitude = "-122.408966";
-
-    $http.get("https://api.foursquare.com/v2/venues/search?client_id=1RHHUREOVHBA2AJNFQOL4KDNFARPEILMDW3AXLVJ30KACPFW&client_secret=EEE40MR4REN231B10GI1QQJ3O3FHAJ2RINJ4WO0HOFPK4WUR&ll=" + latitude + "," + longitude + "&query=" + query + "&v=20140806&m=foursquare")
-      .success(function(response) {
-        $scope.result = response.response;
-        console.log($scope.result);
-      });
-  };
-
+.controller('EventController', function($scope) {
   // Initialize combodate drop-down menus in view
   $(function(){
-      $('#time').combodate({
-          firstItem: 'name',
-          minuteStep: 1
-      }); 
-      $('#time2').combodate({
-          firstItem: 'name', 
-          minuteStep: 1
-      });   
+    $('#time').combodate({
+      firstItem: 'name',
+      minuteStep: 1
+    }); 
+    $('#time2').combodate({
+      firstItem: 'name', 
+      minuteStep: 1
+    });   
   });
 
-  // Initialize location autocomplete (currently not being called appropriately)
-  google.maps.event.addDomListener(window, 'load', function () {
-      var places = new google.maps.places.Autocomplete(document.getElementById('txtPlaces'));
-      google.maps.event.addListener(places, 'place_changed', function () {
-          var place = places.getPlace();
-          var address = place.formatted_address;
-          var latitude = place.geometry.location.lat();
-          var longitude = place.geometry.location.lng();
-          var mesg = "Address: " + address;
-          $('#latitude').text(latitude);
-          $('#latitude').text(longitude);
-      });
+  // Initialize location autocomplete
+  var places = new google.maps.places.Autocomplete(document.getElementById('txtPlaces'));
+  google.maps.event.addListener(places, 'place_changed', function () {
+    var place = places.getPlace();
+    var address = place.formatted_address;
+    var latitude = place.geometry.location.lat();
+    var longitude = place.geometry.location.lng();
+    var mesg = "Address: " + address;
+    $('#latitude').text(latitude);
+    $('#latitude').text(longitude);
   });
 
 })
