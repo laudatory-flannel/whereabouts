5bbde7fdad00c6b5a7d22e3fa2dbf979987b2430
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-10 12:51:28 -0800

    Fully moves logic from AuthController to Auth factory; improves console logs to visually differentiate Facebook and app login

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,27 +1,5 @@
 angular.module('greenfield.auth', ['greenfield.services'])
-.controller('AuthController', function($scope, $location, Facebook, Auth) {
-  $scope.login = function() {
-    if (Auth.isAuth()) {
-      console.log('already logged in to app');
-      $location.path('/home');
-    } else {
-      Facebook.login(function(loginResponse) {
-        Facebook.getUserData(function(userDataResponse) {
-          var accessToken = loginResponse.authResponse.accessToken;
-          var userName = userDataResponse.name;
-          Auth.login({ accessToken: accessToken, userName: userName });
-        });
-      });
-    }
-  };
-
-  $scope.logout = function() {
-    if (Auth.isAuth()) {
-      Auth.logout();
-      $location.path('/auth');
-      Facebook.logout(function() {});
-    } else {
-      console.log('already logged out of app');
-    }
-  }
+.controller('AuthController', function($scope, Auth) {
+  $scope.login = Auth.login;
+  $scope.logout = Auth.logout;
 });
