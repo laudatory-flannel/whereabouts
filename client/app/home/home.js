6d4e45df43ff8f3a97975bc753e5c6d82cf1b80f
gdomorski <gregdomorski@gmail.com>
2016-01-09 22:50:56 -0500

    Working on slide

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -5,13 +5,14 @@
 // random default location in Berkeley, CA
 var DEFAULT_POSITION = [ 30, -120 ];
 
-angular.module('greenfield.home', ['greenfield.services'])
-.controller('HomeController', function($scope, localStorage, $http) {
+var app = angular.module('greenfield.home', ['greenfield.services']);
+app.controller('HomeController', function($scope, localStorage, $http) {
   $scope.map; // google map object
   $scope.loading; // boolean for whether map is loading
   $scope.position = [ null, null ]; // 2-tuple of [ latitude, longitude ]
   $scope.allEvents;
   $scope.allLocations;
+  $scope.postMarker = [];
 
   // Sets $scope position
   $scope.setScopePosition = function(position) {
@@ -52,6 +53,13 @@ angular.module('greenfield.home', ['greenfield.services'])
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   };
 
+
+  $scope.callMarker = function(num){
+    console.log($scope.postMarker);
+      google.maps.event.trigger($scope.postMarker[num], 'click', 'click');
+
+  },
+
   // Renders map in $('#map') DOM element, based on $scope position
   $scope.renderMap = function() {
     $scope.$apply(function() {
@@ -73,13 +81,15 @@ angular.module('greenfield.home', ['greenfield.services'])
 
     var allLocations = [
     {
+      id: 1,
       personname: "Greg Domorski",
-      description: "I'm Starbucks Bros!",
+      description: "I'm at Starbucks Bros!",
       timeuntil: "8 p.m.",
       latitude: 37.793686,
       longitude: -122.401268
     },
     {
+      id: 2,
       personname: "Max O'Connell",
       description: "I'm at SF GreenSpace HACKING! YEAH HACK REACTOR",
       timeuntil: "10 p.m.",
@@ -93,25 +103,30 @@ angular.module('greenfield.home', ['greenfield.services'])
       ['<h4>My People</h4>', 37.789911, -122.402327],
     ];
 
+
     $scope.allLocations = allLocations;
 
     var infowindow = new google.maps.InfoWindow({maxWidth: 160});
+    var postMarker;
+
     for (var i = 0; i < allLocations.length; i++) {  
-      var postMarker = new google.maps.Marker({
+      $scope.postMarker.push(new google.maps.Marker({
         position: new google.maps.LatLng(allLocations[i].latitude, allLocations[i].longitude), 
         animation: google.maps.Animation.DROP,
         map: $scope.map
-      });
+      }));
       
-      google.maps.event.addListener(postMarker, 'click', (function(postMarker, i) {
+
+      google.maps.event.addListener($scope.postMarker[i], 'click', (function(postMarker, i) {
         return function() {
           infowindow.setContent(allLocations[i].personname);
           infowindow.open($scope.map, postMarker);
         };
-      })(postMarker, i));
+      })($scope.postMarker[i], i));
     }
-  };
+    
 
+  };
 
   $scope.initMap = function() {
     $scope.loading = true;
@@ -132,5 +147,96 @@ angular.module('greenfield.home', ['greenfield.services'])
 
 
 
+//   $scope.compile = function(element) {
+//     var el = angular.element(element);    
+//     $scope = el.scope();
+//     $injector = el.injector();
+//     $injector.invoke(function($compile){
+//        $compile(el)($scope);
+//     });  
+//   };
+
+//   $scope.open_panel = function(){
+//     $scope.slideIt();
+//     var el = document.getElementById("sidebar");
+//     el.setAttribute("id", "sidebar1");
+//     el.setAttribute("ng-click", "close_panel()");
+//     $scope.compile(el);
+//   };
+
+//   $scope.slideIt = function(){
+//     var slidingDiv = document.getElementById("slider");
+//     var stopPosition = 0;
+//     if (parseInt(slidingDiv.style.right) < stopPosition) {
+//     slidingDiv.style.right = parseInt(slidingDiv.style.right) + 2 + "px";
+//     setTimeout($scope.slideIt, 1);
+//     }
+//   };
+
+//   $scope.slideIn = function(){
+//     var slidingDiv = document.getElementById("slider");
+//     var stopPosition = -342;
+//     if (parseInt(slidingDiv.style.right) > stopPosition) {
+//       slidingDiv.style.right = parseInt(slidingDiv.style.right) - 2 + "px";
+//       setTimeout($scope.slideIn, 1);
+//     }
+//   };
+
+//   $scope.close_panel = function(){
+//     $scope.slideIn();
+//     el = document.getElementById("sidebar1");
+//     el.setAttribute("id", "sidebar");
+//     el.setAttribute("ng-click", "open_panel()");
+//     $scope.compile(el);
+//   };
 
 });
+
+app.controller('MainCtrl', function ($scope) {
+    $scope.showModal = false;
+    $scope.toggleModal = function(){
+        $scope.showModal = !$scope.showModal;
+    };
+  });
+
+app.directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' + 
+          '<div class="modal-dialog">' + 
+            '<div class="modal-content">' + 
+              '<div class="modal-header">' + 
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                '<h4 class="modal-title">{{ title }}</h4>' + 
+              '</div>' + 
+              '<div class="modal-body" ng-transclude></div>' + 
+            '</div>' + 
+          '</div>' + 
+        '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
+
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+  });
