1d056e091dfa8be82b7137a6312da9242d4c4374
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-10 20:19:18 -0800

    Fixes bug with controllers being declared twice

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -55,7 +55,6 @@ app.controller('HomeController', function($scope, localStorage, $http) {
       disableDefaultUI: true,
       styles: [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
     });
-
   };
 
   $scope.setUpRoutes = function() {
@@ -91,7 +90,6 @@ app.controller('HomeController', function($scope, localStorage, $http) {
     };
 
     // Register event listeners to display route whenever endpoints change
-    // console.log('outside the func', $scope);
     document.getElementById('start').addEventListener('change', displayRoute);
     document.getElementById('end').addEventListener('change', displayRoute);
   };
@@ -162,13 +160,13 @@ app.controller('HomeController', function($scope, localStorage, $http) {
       var marker = addMarker(location.latitude, location.longitude, EVENT_ICON_URL);
       $scope.postMarker.push(marker);
       
-      google.maps.event.addListener($scope.postMarker[i], 'click', (function(postMarker, i) {
+      google.maps.event.addListener($scope.postMarker[i], 'click', (function(marker, i) {
         return function() {
           infowindow.setContent(
             "<h4>" + $scope.allLocations[i].personname +
             "</h4>" + $scope.allLocations[i].description + 
             "<p>Will be there until " + $scope.allLocations[i].timeuntil + "</p>");
-          infowindow.open($scope.map, postMarker);
+          infowindow.open($scope.map, marker);
         };
       })($scope.postMarker[i], i));
     }
