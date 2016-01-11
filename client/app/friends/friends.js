aba87e7b6d15c2ec4dd22f81d76f6dd748764373
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-11 11:27:06 -0800

    Refactors Friends to use user id in localStorage

--- client/app/friends/friends.js
+++ client/app/friends/friends.js
@@ -1,22 +1,15 @@
-//used purely for testing - production code will not utilize a default user
-var DEFAULT_USER = {
-  _id: "569330315d957f2fd73cd7e2",
-  name: 'Rachel',
-  friends: []
-};
-
 angular.module('greenfield.friends', ['greenfield.services'])
 .factory('Friends', function ($http) {
 
-  var modifyFriend = function (user, friend, action) {
-    console.log('modifying friend', user._id)
+  var modifyFriend = function (userId, friend, action) {
+    console.log('modifying friend', userId)
     data = {
       friend: friend,
       action: action
     }
     return $http({
       method: 'POST',
-      url: '/users/' + user._id + '/friends',
+      url: '/users/' + userId + '/friends',
       data: data
     })
     .then(function (resp) {
@@ -51,22 +44,22 @@ angular.module('greenfield.friends', ['greenfield.services'])
     getAllUsers: getAllUsers
   };
 })
-.controller('FriendsController', function ($scope, localStorage, HTTP, Friends) {
-  $scope.user = localStorage.get('flannel.user') || DEFAULT_USER;
+.controller('FriendsController', function ($scope, localStorage, Friends) {
+  $scope.userId = localStorage.get('flannel._id');
   $scope.friends = [];
   $scope.users = [];
 
   $scope.addFriend = function(friend) {
     console.log('calling $scope.addFriend', friend)
     $scope.search = null;
-    Friends.modifyFriend($scope.user, friend, 'add')
+    Friends.modifyFriend($scope.userId, friend, 'add')
     .then(function(data) {
       $scope.friends.push(friend);
     });
   };
 
   $scope.getAllFriends = function() {
-    Friends.getAllFriends($scope.user._id)
+    Friends.getAllFriends($scope.userId)
     .then(function(result) {
       $scope.friends = result.data;
     });
@@ -80,7 +73,7 @@ angular.module('greenfield.friends', ['greenfield.services'])
   };
 
   $scope.deleteFriend = function(friend) {
-   Friends.modifyFriend($scope.user, friend, 'delete')
+   Friends.modifyFriend($scope.userId, friend, 'delete')
    .then(function(data) {
      for (var i = 0; i < $scope.friends.length; i++) {
       if ($scope.friends[i] === friend) {
