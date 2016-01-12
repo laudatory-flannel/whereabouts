37eb7adb2e26a7fb5afdded335371b2661f2a26e
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-11 17:03:58 -0800

    Factors out dummy data and standardizes comments in HomeController

--- client/app/home/controllers/home.js
+++ client/app/home/controllers/home.js
@@ -2,7 +2,7 @@ var USER_ICON_URL = 'img/currentlocation.png';
 var EVENT_ICON_URL = 'img/flag.png';
 var DEFAULT_POSITION = [ 37.784, -122.409 ]; // Hack Reactor
 
-homeModule.controller('HomeController', function($scope, Map, Directions, Markers, HTTP, User) {
+homeModule.controller('HomeController', function($scope, Map, Directions, Markers, HTTP, User, DummyData) {
   $scope.map; // google map object
 
   $scope.initMap = function(callback) {
@@ -52,9 +52,80 @@ homeModule.controller('HomeController', function($scope, Map, Directions, Marker
     // Add marker for user
     Markers.addUserMarker($scope.map, Map.getLocalPosition());
     
-    // ---- seed ---- (for testing purposes)
-    // This entire section should be deleted for production
-    $scope.allEvents = [
+    // Seeds events with dummy data and adds corresponding markers
+    // This should be deleted for production
+    $scope.allEvents = DummyData;
+    _.forEach($scope.allEvents, function(event) {
+      var marker = Markers.addEventMarker($scope.map, event);
+      $scope.markers.push(marker);
+    });
+  };
+
+  // Triggers click on marker from click on event in feed
+  $scope.callMarker = function($index){
+    var marker = $scope.markers[$index];
+    Markers.triggerClick(marker);
+  };
+
+  // Clears all markers from map - only used for testing purposes now
+  // Eventually may be utilized for removing expired events
+  $scope.clearMarkers = function() {
+    _.forEach($scope.markers, function(marker) {
+      Markers.removeFromMap(marker);
+    });
+  };
+
+  // Gets the events from the server
+  $scope.getEvents = function(callback){
+    HTTP.sendRequest('GET', '/events')
+    .then(function(response) {
+      var events = response.data;
+
+      // Performs client-side filtering for events a user should see
+      // (i.e. public events, user's own events, and friend's events)
+      events = _.filter(events, function(event) {
+        return event.isPublic ||
+               event.userId === User.getId() ||
+               _.pluck($scope.friends, '_id').indexOf(event.userId) !== -1;
+      });
+
+      callback(events);
+    });
+  };
+
+  // Updates $scope.allEvents with new events and adds markers accordingly
+  // Currently never removes events, even if inactive
+  $scope.updateEvents = function(events) {
+    _.forEach(events, function(event) {
+      if (_.pluck($scope.allEvents, '_id').indexOf(event._id) === -1) {
+        $scope.allEvents.push(event);
+        console.log('got new event from server:', event);
+
+        //add marker for event
+        var marker = Markers.addEventMarker($scope.map, event);
+        $scope.markers.push(marker);
+      }
+    });
+  };
+
+  // Get user's friends, to be able to filter events
+  // Assumes friends do not change during their visit to the page
+  User.getFriends()
+  .then(function(response) {
+    $scope.friends = response.data;
+    $scope.initMap(function() { //finishes asynchronously
+      $scope.initRoutes();
+      $scope.initMarkers();
+      setInterval(function(){
+        $scope.getEvents(function(events) {
+          $scope.updateEvents(events);
+        });
+      }, 1000);
+    });
+  });
+})
+.factory('DummyData', function() {
+  return [
     {
       id: 1,
       userName: "Greg Domorski",
@@ -86,94 +157,7 @@ homeModule.controller('HomeController', function($scope, Map, Directions, Marker
       endedAt: "8 p.m.",
       location: { coordinates: [ -122.406435, 37.784118 ] },
       isPublic: true
-    },    
-    ];
-
-    // Add event marker for each location
-    _.forEach($scope.allEvents, function(event) {
-      var marker = Markers.addEventMarker($scope.map, event);
-      $scope.markers.push(marker);
-    });
-
-    // ---- end seed ----
-  };
-
-  // Triggers click on marker from click on event in feed
-  $scope.callMarker = function($index){
-    var marker = $scope.markers[$index];
-    Markers.triggerClick(marker);
-  };
-
-  // Clears all markers from map - only used for testing purposes now
-  // Eventually may be utilized for removing expired events
-  $scope.clearMarkers = function() {
-    _.forEach($scope.markers, function(marker) {
-      Markers.removeFromMap(marker);
-    });
-  };
-
-  //gets the events from the server
-  $scope.getEvents = function(callback){
-    HTTP.sendRequest('GET', '/events')
-    .then(function(response) {
-      var events = response.data;
-
-      //client-side filtering for events a user can see
-      //if isPublic OR is user's own even OR userId is in user.friends
-      events = _.filter(events, function(event) {
-        return event.isPublic ||
-               event.userId === User.getId() ||
-               _.pluck($scope.friends, '_id').indexOf(event.userId) !== -1;
-      });
-
-      callback(events);
-    });
-  };
-
-  // Updates $scope.allEvents with new events and adds markers accordingly
-  // Currently never removes events, even if inactive
-  $scope.updateEvents = function(events) {
-    _.forEach(events, function(event) {
-      if (_.pluck($scope.allEvents, '_id').indexOf(event._id) === -1) {
-        console.log('pushing new event', event);
-        // console.log(event.isPublic)
-        // console.log(event.userId === User.getId());
-        // console.log(_.pluck($scope.friends, '_id').indexOf(event.userId) !== -1);
-        $scope.allEvents.push(event);
-
-        //add marker for event
-        var marker = Markers.addEventMarker($scope.map, event);
-        $scope.markers.push(marker);
-      }
-    });
-  };
-
-
-  //Get user's friends, to be able to filter events
-  //Assumes friends do not change during their visit to the page
-  User.getFriends()
-  .then(function(response) {
-    $scope.friends = response.data;
-    $scope.initMap(function() { //finishes asynchronously
-      $scope.initRoutes();
-      $scope.initMarkers();
-      setInterval(function(){
-        $scope.getEvents(function(events) {
-          $scope.updateEvents(events);
-        });
-      }, 1000);
-    });
-
-    //initial 
-    $scope.initMap(function() { //finishes asynchronously
-      $scope.initRoutes();
-      $scope.initMarkers();
-      setInterval(function(){
-        $scope.getEvents(function(events) {
-          $scope.updateEvents(events);
-        });
-      }, 1000);
-    });
-  });
+    }
+  ];
 });
 
